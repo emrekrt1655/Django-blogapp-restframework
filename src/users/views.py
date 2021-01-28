23014f3bff04a9835b84d8e4c8ad95a5d2caf52b
Emre <emrekurtt1655@gmail.com>
2021-01-28 13:55:47 +0100

    08-BE-Create Authentication systems and give Views authentication and…

--- src/users/views.py
+++ src/users/views.py
@@ -7,26 +7,32 @@ from users.models import Profile
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import RegisterSerializer, ProfileSerializer, UserSerializer, ProfileUpdateSerializer, ProfileDetailSerializer
-
+from rest_framework.permissions import IsAdminUser, AllowAny, IsAuthenticated
+from .permissions import IsOwner
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
-    # permission_classes = (AllowAny,)
+    permission_classes = [AllowAny]
     serializer_class = RegisterSerializer
     
 class ProfileListView(generics.ListAPIView):
     queryset = Profile.objects.all()
     serializer_class=ProfileSerializer
+    permission_classes = [IsAdminUser]
+
 
 class DetailedProfileView(generics.RetrieveAPIView):
     queryset = Profile.objects.all()
     serializer_class=ProfileDetailSerializer
     lookup_field = 'slug'
+    permission_classes = [IsAuthenticated, IsOwner]
+
     
 class ProfileUpdateView(generics.RetrieveUpdateAPIView):
     queryset = Profile.objects.all()
     serializer_class = ProfileUpdateSerializer
     lookup_field = 'slug'
+    permission_classes = [IsAuthenticated, IsOwner]
 
     def perform_update(self, serializer):
         serializer.save()
